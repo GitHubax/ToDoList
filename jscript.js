3d23ddeee02d0645424513a3bac97f6aa087ab50
GitHubax <102989683+GitHubax@users.noreply.github.com>
2022-05-27 17:37:20 +0530

    basic functioning of the app almost ready

--- jscript.js
+++ jscript.js
@@ -1,13 +1,11 @@
 var x = 0;
-var a = new Array();
+var a = new Array();                                   //task content storage array
+//var deletion = new Array();
 
-//task strikeout
+
 //editable task
 //local storage
 //task deletion
-//all done button
-//all del button with alert + taskhead invisibility
-
 
 
 function samecheck(){                                 //to check if a task is previously added or not
@@ -20,10 +18,10 @@ function samecheck(){                                 //to check if a task is pr
 }
 
 
-function show(){
+function show(){            //to create a task on button click
    
    var value = document.getElementById("inputbox").value;             //task input content extraction
-   a[x] = value;
+   a[x] = value;                                                    //value globalization for samecheck()
    
 
    if (value == "" ){
@@ -31,7 +29,8 @@ function show(){
    }
 
    else if(samecheck() != 0){
-      x++;            //this is as a task counter also used to change task id as Task 1, Task 2...
+
+      x++;                  //this is as a task counter also used to change task id as Task 1, Task 2...
 
       //createElement creates an element
       //getElementById tags an element by its id, similar for class
@@ -39,25 +38,87 @@ function show(){
       //appendChild creates a child element
       //abcd.xyz----here abcd is the var and xyz is property
 
-      document.getElementById('taskhead').style.display = 'block';     //taskhead visibility syntax
+      document.getElementById('taskhead').style.display = 'block';     //taskhead visibility toggle on
+
       var checkbox = document.createElement('input');
       checkbox.type = "checkbox";                                      //checkbox creation
       var tsk = "Task " + x;                                           //unique id for each task
       checkbox.id = (tsk) ;
+      checkbox.name = "chk";
 
-      var lbl = document.createElement('label');                      //label for checkbox (taskname)
+      var lbl = document.createElement('label');                   //create label for checkbox (taskname)
       lbl.htmlFor = (tsk);                                            //checkbox linking
       lbl.append(value);                                              //Task input content injection
 
+      var del = document.createElement('input');                      //create a delete task btn
+      del.type = 'reset';
+      del.value = 'X';
+      //deletion[x] = "Delbtn " + x;
+      //del.id = (deletion[x]);
+      del.className = "taskdelbtn";
+      
       var taskdiv = document.createElement("div");                    //div wrapper for each task
       taskdiv.className = "Tasks";
       taskdiv.appendChild(checkbox);                                  //child element 'checkbox' creation
       taskdiv.appendChild(lbl);                                       //child element 'lbl' creation
+      taskdiv.appendChild(del);
 
       var br = document.createElement('br');                          //<br> tag creation
       
-      var container = document.getElementById("Tasks");         //final insertion within empty div 'Tasks'
+      var container = document.getElementById("Tasks");               //insertion within empty div 'Tasks'
       container.appendChild(taskdiv);                                 //content insertion
       container.appendChild(br);                                      //breakline insertion   
    }  
 }
+
+
+
+
+// for (i=0;i<x;i++){
+// document.getElementById(deletion[i]).onclick = function() {deltask()};
+// function deltask(){
+
+//    var tsk = "Task " + i; 
+//    document.getElementById(tsk).innerHTML = "";
+// }
+// }
+
+
+
+
+
+function delall(){                                                     //func to del all task
+
+   alert("Are you sure you want to delete all tasks forever?");        //alert for delete all task
+   document.getElementById('taskhead').style.display = 'none';         //taskhead visibility toggle off
+   document.getElementById("Tasks").innerHTML = "";                    //tasklist clearance
+   x = 0;                                                              //counter reset
+}
+
+
+function checkall(){                                                   //func to check all chkboxes
+
+   document.getElementById('chk').style.display = "none";              //mark-done btn toggle invisible
+   document.getElementById('unchk').style.display = "inline-flex";     //mark-undone btn toggle visible
+   //document.getElementById('Tasks').style.opacity = "50%";           //div opacity down to 50%
+   var ele = document.getElementsByName('chk');                        //taking the checkboxes in acc
+   for(var i=0; i<ele.length; i++){
+      if(ele[i].type=='checkbox')
+         ele[i].checked=true;                                          //all chkbox checked syntax
+   }           
+}
+
+
+function uncheckall(){                                                 //func to uncheck all chkboxes
+
+   document.getElementById('unchk').style.display = "none";            //mark-undone btn toogle invisible
+   document.getElementById('chk').style.display = "inline-flex";       //mark-done button toggle visible
+   //document.getElementById('Tasks').style.opacity = "100%";          //div opacity back to 100%
+   var ele = document.getElementsByName('chk');                        //taking the checkboxes in acc
+   for(var i=0; i<ele.length; i++){  
+      if(ele[i].type=='checkbox')  
+            ele[i].checked=false;                                      //all chkbox unchecked syntax
+   }
+}
+
+
